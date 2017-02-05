ebd7a4fa5b62d59001ff87d740ae575c965bbb7b
Jari Pekkala <salomoni@outlook.com>
2017-02-05 11:11:41 +0200

    remove smooth scorlling

--- js/main.js
+++ js/main.js
@@ -24,42 +24,4 @@
     return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
   }
 
-
-  document.querySelectorAll('.hero__nav__item').forEach(item => {
-    item.addEventListener('click', e => scrollIt(e.target));
-  });
-
-  
-
-  function scrollIt(element, duration = 200) {
-    function checkBody() {
-      document.documentElement.scrollTop += 1;
-      const body = (document.documentElement.scrollTop !== 0) ? document.documentElement : document.body;
-      document.documentElement.scrollTop -= 1;
-      return body;
-    }
-
-    const body = checkBody();
-    const start = body.scrollTop;
-    const startTime = Date.now();
-
-    const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-    let destination = documentHeight - element.offsetTop < windowHeight ? documentHeight - windowHeight : element.offsetTop;
-
-    destination += documentHeight;
-
-    function scroll() {
-      const now = Date.now();
-      const time = Math.min(1, ((now - startTime) / duration));
-      body.scrollTop = (time * (destination - start)) + start;
-
-      if (body.scrollTop === destination || (window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
-        return;
-      }
-      requestAnimationFrame(scroll);
-    }
-    scroll();
-  }
-
 })();
