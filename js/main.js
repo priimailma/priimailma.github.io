a24a9932a5a0ed3f32defc26d029a2bfea951ee9
Jari Pekkala <salomoni@outlook.com>
2017-02-19 16:56:22 +0200

    refactor a bit, fix bug with menu

--- js/main.js
+++ js/main.js
@@ -2,34 +2,47 @@
   
   var menu = document.querySelector('.menu');
   var hero = document.querySelector('.hero');
-  var parallax = document.querySelectorAll('.parallax'),
-      speed = 0.5;
 
+  // parallax options
+  var parallax = document.querySelectorAll('.parallax'),
+      speed = 0.6;
+
+  // Run when the user scrolls
   window.onscroll = function() {
+    showOrHideMenu();
+    parallaxElements();
+  };
+
+  // Show the static menu if scrolled beyond the hero section
+  function showOrHideMenu() {
     if (isVisibleOnScreen(hero)) {
       menu.classList.remove('menu--visible');
     } else {
       menu.classList.add('menu--visible');
     }
+  }
 
-    [].slice.call(parallax).forEach(function(el,i) {
+  // Move the bg of elements in parallax effect if visible
+  function parallaxElements() {
+    [].slice.call(parallax).forEach(function(el) {
       if (! isVisibleOnScreen(el)) {
         return;
       }
 
-      var windowYOffset = window.pageYOffset;
-      var elBackgrounPos = '50% ' + parseInt(windowYOffset * speed) + 'px';
-
-      el.style.backgroundPosition = elBackgrounPos;
+      el.style.backgroundPosition = '50% ' + parseInt(window.pageYOffset * speed) + 'px';
     });
-  };
-
+  }
   
-
-  function isVisibleOnScreen(elm) {
-    var rect = elm.getBoundingClientRect();
+  // Check is the given el visible on the screen
+  // with optional threshold.
+  function isVisibleOnScreen(elm, threshold) {
     var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+    var rect = elm.getBoundingClientRect();
+
+    return ! (
+      rect.bottom - (threshold || 0) < 0 || 
+      rect.top - viewHeight >= 50
+    );
   }
 
 })();
